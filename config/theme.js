f9cdd29841a6386acab2f09c4a850c35703db5dd
setyarp <setyarizkypradana@gmail.com>
2023-07-30 09:09:16 +0700

    integrated with MUI

---
+++ config/theme.js
@@ -0,0 +1,27 @@
+import { Poppins } from 'next/font/google'
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+// Create a theme instance.
+const poppins = Poppins({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+    typography: {
+        fontFamily: poppins.style.fontFamily,
+    },
+});
+export default theme;
