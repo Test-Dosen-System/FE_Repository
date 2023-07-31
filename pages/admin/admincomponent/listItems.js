e61e699348183b0b34119feb0ab724afa0a0cc58
setyarp <setyarizkypradana@gmail.com>
2023-07-31 07:18:09 +0700

    setup dashboard admin

---
+++ pages/admin/admincomponent/listItems.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Link from '@mui/material/Link';
+
+export default function mainListItems() {
+  return (
+    <>
+      <ListItemButton>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <Link color="inherit" href="/admin/dashboard" underline='none'>
+          <ListItemText primary="Dashboard" />
+        </Link>
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <NoteAddIcon />
+        </ListItemIcon>
+        <Link color="inherit" href="/admin/createAssesment" underline='none'>
+          <ListItemText primary="Buat Soal" />
+        </Link>
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Daftar Soal" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <LogoutIcon />
+        </ListItemIcon>
+        <ListItemText primary="Logout" />
+      </ListItemButton>
+    </>
+  )
+};
+
---
+++ pages/admin/createAssesment/index.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Navbar from '../admincomponent/navbar'
+
+const defaultTheme = createTheme();
+
+export default function CreateAssesment() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            Assesment Here
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
